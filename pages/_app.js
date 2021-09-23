510ef3bac7c9f453f014cd627b4de9fa2f45889b
MarcisUrbans <urbans.marcis@gmail.com>
2021-09-23 22:45:10 +0300

    fixed vercel error v2

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout/Layout.js";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  //tests
   return (
     <Layout>
       <Component {...pageProps} />
